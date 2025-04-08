2c721e5f1f2a736506a6aa96a87c38c82ead456d
Rajarj16 <iamrj.raja@gmail.com>
2025-04-08 11:13:31 +0200

    Cypress Cloud update - Inprogress

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "s8mevm",
     baseUrl: "http://localhost:3001",
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    setupNodeEvents(on, config)
+    {
+      
     },
   },
 });
